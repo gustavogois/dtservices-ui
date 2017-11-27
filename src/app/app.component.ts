5ed3a6145d152cea106bce67fe60c23712692cd6
Gustavo Gois <gustavo.gois@gmail.com>
2017-11-27 01:10:03 +0000

    Tipo de Servico - Tabela de Dados - Formatação com ngTemplate e botões

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   tiposDeServico = [
-    { nome: 'Tipo de Serviço 01', descricao: 'Descrição para o tipo de serviço 01', valor: 10},
+    { nome: 'Tipo de Serviço 01', descricao: 'Descrição para o tipo de serviço 01', valor: -10},
     { nome: 'Tipo de Serviço 02', descricao: 'Descrição para o tipo de serviço 02', valor: 20},
     { nome: 'Tipo de Serviço 03', descricao: 'Descrição para o tipo de serviço 03', valor: 30},
     { nome: 'Tipo de Serviço 04', descricao: 'Descrição para o tipo de serviço 04', valor: 40},
